83edeef659d5414feaffe940048ff628dd76034a
Rich Fellure <rfellure24@gmail.com>
2015-03-06 09:58:47 -0600

    Initial Commit

---
+++ RFZodiacExt/RFZodiacExt.h
@@ -0,0 +1,19 @@
+//
+//  RFZodiacExt.h
+//  RFZodiacExt
+//
+//  Created by Rich Fellure on 3/6/15.
+//  Copyright (c) 2015 RichApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RFZodiacExt.
+FOUNDATION_EXPORT double RFZodiacExtVersionNumber;
+
+//! Project version string for RFZodiacExt.
+FOUNDATION_EXPORT const unsigned char RFZodiacExtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RFZodiacExt/PublicHeader.h>
+
+
