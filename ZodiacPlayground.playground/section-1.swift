eced6653b9fef056b2c21c3e900dc11517a54b71
Rich Fellure <rfellure24@gmail.com>
2015-03-06 10:50:14 -0600

    Addition of method to take a date and return the Chinese Animal
    Use of an enum to return the Animal type, which can give the string value for that animal. Latest date is 1924 that can be checked.

--- ZodiacPlayground.playground/section-1.swift
+++ ZodiacPlayground.playground/section-1.swift
@@ -1,12 +1,6 @@
-//
-//  NSDateExt.swift
-//  RFZodiacExt
-//
-//  Created by Rich Fellure on 3/6/15.
-//  Copyright (c) 2015 RichApps. All rights reserved.
-//
+// Playground - noun: a place where people can play
 
-import Foundation
+import UIKit
 
 enum Animal : Int {
     case Rat
@@ -41,31 +35,7 @@ enum Animal : Int {
 }
 
 extension NSDate {
-
-    convenience init(string: String) {
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = "MMM d"
-        let date = formatter.dateFromString(string)
-        self.init(timeInterval: 0, sinceDate:date!)
-    }
-
-    func getZodiacSign()-> String {
-        let signs = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
-
-        let dates = ["March 20", "April 19", "May 20", "June 20", "July 22", "August 22", "September 23", "October 22", "November 21", "December 21", "January 19", "February 18"]
-        for i in 0...11{
-            let date = NSDate(string: dates[i])
-            let second = i < 11 ? dates[i + 1] : dates[0] as String
-            let sD = NSDate(string: second)
-            let d = self.formatDate()
-            if d.compare(date) == NSComparisonResult.OrderedDescending && d.compare(sD) == .OrderedAscending {
-                return signs[i]
-            }
-        }
-        return ""
-    }
-
-    func chineseZodiacAnimal()-> Animal {
+    func chineseZodiacAnimal()-> String {
         let startS = "1924"
         let f = NSDateFormatter()
         f.dateFormat = "yyyy"
@@ -73,16 +43,9 @@ extension NSDate {
         let d = self.formatDateForChineseYear()
         let interv = d.timeIntervalSinceDate(s!)
         let y = Int(interv / 60 / 60 / 24 / 365)
-        let animal = Animal(rawValue: y % 12)!
-        return animal
-    }
-
-    private func formatDate()-> NSDate {
-        let df = NSDateFormatter()
-        df.dateFormat = "MMM d"
-        let s = df.stringFromDate(self)
-        let d = NSDate(string: s)
-        return d
+        let m = y % 12
+        let animal = Animal(rawValue: m)!
+        return animal.string()
     }
 
     private func formatDateForChineseYear()-> NSDate {
@@ -100,3 +63,6 @@ extension NSDate {
         self.init(timeInterval: 0, sinceDate:date!)
     }
 }
+
+let date = NSDate()
+date.chineseZodiacAnimal()
