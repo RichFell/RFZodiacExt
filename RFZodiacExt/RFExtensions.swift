33f780a4027ac8cfd65e841081036d21ebd9257d
Rich Fellure <rfellure24@gmail.com>
2015-03-06 15:05:46 -0600

    Addition of strengths and weaknesses of the Zodiac signs

--- RFZodiacExt/RFExtensions.swift
+++ RFZodiacExt/RFExtensions.swift
@@ -90,30 +90,30 @@ public enum Animal : Int {
     */
     public func weaknesses()->String {
         switch self {
-        case Rat:
-            return "They are usually timid, lack of concentration and stability. They are in need of courage introspection, principles and responsibility. The do not look authoritatively, incapable of assuming a leading position. They stick fast and refused to be influenced. They are easily guided only by their private interests. They have natural goodness but look unpolite. They are also iobstinate and selfish. They are likely to speculate and they profess their greed as a virtue."
-        case .Ox:
-            return "They are obstinate and poor at communication. Leaders in their career may not discover their abilities. They looks prudish and distant, but not feminine, charming and lively enough. They usually do not get a great facility in speaking, being silent and persistent in their old ways. In their old age, they would be usually bestowed with happiness."
-        case .Tiger:
-            return "Tigers usually tend to show off before others. They are given to deep thinking and capable of great sympathy, however, they can be short-tempered without interest with long-term endeavor. Sometimes, they come into conflict easily with the seniors and people in authority. Their hasty mind and decision may cause poor results. Weakness of characteristics such as over confident and traitorous may result in unsuccessful communication and cooperation with others."
-        case .Rabbit:
-            return "They lack meditative abilities and often sink money into ideas that may cause failures in their career. They are amorous and not determined, soft in appearance and stubborn inside. They usually do not submit to dreary life, but create romance. They are not good at delving deeply and likely to escape the reality. They may lose good chances because of reserved characteristic traits. "
-        case .Dragon:
-            return "They are a little arrogant and impatient, while dragon women can be over- confident. Sometimes, 'dragons' are unable to control their moods very well due to being eccentric, tactless, fiery, intolerant and unrealistic. They may feel blank about the future. There is no lack of romance in their life over all, but they seldom give true love. Therefore, they do not usually show disappointment in love. They may criticize others for their inefficiency at work. They have a nature ardent to excess and couldn't withstand setbacks which make them flee away. If they overcome these defects, they can have a brighter future."
-        case .Snake:
-            return "They are likely to be jealous and suspicious. They should be cautious about what they discuss with others, as it might cause them to lose friendship and opportunities. They tend to overdo things. They prefer to rely on themselves and have doubts about other people's judgment. They are courteous  with polite manners, but they can be headstrong. They are fickle and usually have problems in relationships or marriage problems."
-        case .Horse:
-            return "Loving spending is their problem, for they need a great amount of money to support social activities and decent dressing. Besides, frankness allows them to let out secret easily. Persistence is what they lack on the path leading to success."
-        case .Goat:
-            return "indecisive, timid, vain, pessimistic, moody, weak-willed"
-        case .Monkey:
-            return "They are probably to be successful but they are sometimes agreeable. If they can get into something immediately, they will be discouraged. Their shortcomings on the characteristics are jealous, suspicious, cunning, selfish and arrogant sometimes. They tend to look down upon others. If they are not impatient and mouthy, they can gain more achievement."
-        case .Rooster:
-            return "They might be enthusiastic about something quickly, but soon might be impassive. They are often a bit eccentric and have difficulty relating with others. They are critical and think they are always right. Like their fortunes, their emotions swing from very high very slow. They can be positive but selfish, caustic and too outspoken. They are narrow-minded and vain. They probably refuse suggestions but would like to lecture others. If they can overcome their arrogance being sincerely in speaking, they will make more progress. "
-        case .Dog:
-            return "Most women under this sign are appealing but lack stability. They can be cold emotionally and sometimes distant at parties. They can find fault with many things and are noted for their sharp tongues. Their characteristics are irritable and usually bothered by unwarranted anxiety. They sometimes try to flaunt and be brave. They are likely to be an armchair theorist deficient in practical experience."
-        case .Pig:
-            return "Shortcomings on their characteristics are trusting and sometimes naïve, so they may easily fall in traps. Although they are loyal to friends, they do not have many friends. They are hot-tempered and impulsive. They are not good at communicating with others. If they play pranks on people, they will not know when to stop"
+            case Rat:
+                return "They are usually timid, lack of concentration and stability. They are in need of courage introspection, principles and responsibility. The do not look authoritatively, incapable of assuming a leading position. They stick fast and refused to be influenced. They are easily guided only by their private interests. They have natural goodness but look unpolite. They are also iobstinate and selfish. They are likely to speculate and they profess their greed as a virtue."
+            case .Ox:
+                return "They are obstinate and poor at communication. Leaders in their career may not discover their abilities. They looks prudish and distant, but not feminine, charming and lively enough. They usually do not get a great facility in speaking, being silent and persistent in their old ways. In their old age, they would be usually bestowed with happiness."
+            case .Tiger:
+                return "Tigers usually tend to show off before others. They are given to deep thinking and capable of great sympathy, however, they can be short-tempered without interest with long-term endeavor. Sometimes, they come into conflict easily with the seniors and people in authority. Their hasty mind and decision may cause poor results. Weakness of characteristics such as over confident and traitorous may result in unsuccessful communication and cooperation with others."
+            case .Rabbit:
+                return "They lack meditative abilities and often sink money into ideas that may cause failures in their career. They are amorous and not determined, soft in appearance and stubborn inside. They usually do not submit to dreary life, but create romance. They are not good at delving deeply and likely to escape the reality. They may lose good chances because of reserved characteristic traits. "
+            case .Dragon:
+                return "They are a little arrogant and impatient, while dragon women can be over- confident. Sometimes, 'dragons' are unable to control their moods very well due to being eccentric, tactless, fiery, intolerant and unrealistic. They may feel blank about the future. There is no lack of romance in their life over all, but they seldom give true love. Therefore, they do not usually show disappointment in love. They may criticize others for their inefficiency at work. They have a nature ardent to excess and couldn't withstand setbacks which make them flee away. If they overcome these defects, they can have a brighter future."
+            case .Snake:
+                return "They are likely to be jealous and suspicious. They should be cautious about what they discuss with others, as it might cause them to lose friendship and opportunities. They tend to overdo things. They prefer to rely on themselves and have doubts about other people's judgment. They are courteous  with polite manners, but they can be headstrong. They are fickle and usually have problems in relationships or marriage problems."
+            case .Horse:
+                return "Loving spending is their problem, for they need a great amount of money to support social activities and decent dressing. Besides, frankness allows them to let out secret easily. Persistence is what they lack on the path leading to success."
+            case .Goat:
+                return "indecisive, timid, vain, pessimistic, moody, weak-willed"
+            case .Monkey:
+                return "They are probably to be successful but they are sometimes agreeable. If they can get into something immediately, they will be discouraged. Their shortcomings on the characteristics are jealous, suspicious, cunning, selfish and arrogant sometimes. They tend to look down upon others. If they are not impatient and mouthy, they can gain more achievement."
+            case .Rooster:
+                return "They might be enthusiastic about something quickly, but soon might be impassive. They are often a bit eccentric and have difficulty relating with others. They are critical and think they are always right. Like their fortunes, their emotions swing from very high very slow. They can be positive but selfish, caustic and too outspoken. They are narrow-minded and vain. They probably refuse suggestions but would like to lecture others. If they can overcome their arrogance being sincerely in speaking, they will make more progress. "
+            case .Dog:
+                return "Most women under this sign are appealing but lack stability. They can be cold emotionally and sometimes distant at parties. They can find fault with many things and are noted for their sharp tongues. Their characteristics are irritable and usually bothered by unwarranted anxiety. They sometimes try to flaunt and be brave. They are likely to be an armchair theorist deficient in practical experience."
+            case .Pig:
+                return "Shortcomings on their characteristics are trusting and sometimes naïve, so they may easily fall in traps. Although they are loyal to friends, they do not have many friends. They are hot-tempered and impulsive. They are not good at communicating with others. If they play pranks on people, they will not know when to stop"
 
         }
     }
@@ -127,33 +127,30 @@ public enum Animal : Int {
         switch self {
             case .Rat:
                 return "blue, golden, green"
-        case .Ox:
-            return "blue, red, purple"
-        case .Tiger:
-            return "blue, grey, white, orange"
-        case .Rabbit:
-            return "red, pink, purple, blue"
-        case .Dragon:
-            return "golden, silver, hoary"
-        case .Snake:
-            return "red, light yellow, black"
-        case .Horse:
-            return "brown, yellow, purple"
-        case .Goat:
-            return "green, red, purple"
-        case .Monkey:
-            return "white, golden, blue"
-        case .Rooster:
-            return "golden, brown, brownish yellow, yellow"
-        case .Dog:
-            return "green, red, purple"
-        case .Pig:
-            return "yellow, grey, brown, golden"
+            case .Ox:
+                return "blue, red, purple"
+            case .Tiger:
+                return "blue, grey, white, orange"
+            case .Rabbit:
+                return "red, pink, purple, blue"
+            case .Dragon:
+                return "golden, silver, hoary"
+            case .Snake:
+                return "red, light yellow, black"
+            case .Horse:
+                return "brown, yellow, purple"
+            case .Goat:
+                return "green, red, purple"
+            case .Monkey:
+                return "white, golden, blue"
+            case .Rooster:
+                return "golden, brown, brownish yellow, yellow"
+            case .Dog:
+                return "green, red, purple"
+            case .Pig:
+                return "yellow, grey, brown, golden"
         }
-
     }
-
-    //TODO: Add in the description of the different animal years.
 }
 
 
@@ -194,6 +191,64 @@ public enum Zodiac : Int {
             case .Capricorn: return "Capricorn"
             case .Aquarius: return "Aquarius"
             case .Pisces: return "Pisces"
+        }
+    }
+
+    public func strengths()->String {
+        switch self {
+            case .Aries:
+                return "Courage, determination, self-confidence, enthusiasm."
+            case .Taurus:
+                return "Dependable, patient, musical, practical."
+            case .Gemini:
+                return "Curiosity, ability to share ideas, adaptable, affectionate, kind."
+            case .Cancer:
+                return "Compassion, emotional sensitivity, fierce protection of loved ones, tenacity."
+            case .Leo:
+                return "Warmth, humor, pride, joy, creativity, passion, generosity."
+            case .Virgo:
+                return "Practical, loyal, hardworking, analytical, kind."
+            case .Libra:
+                return "Social, fair-minded, cooperative, diplomatic, gracious."
+            case .Scorpio:
+                return "Passionate, stubborn, resourceful, brave, a true friend."
+            case .Sagittarius:
+                return "Great sense of humor, idealistic, generous."
+            case .Capricorn:
+                return "Responsible, good managers, disciplined, self-control, dark sense of humor."
+            case .Aquarius:
+                return "Progressive, original, humanitarian, independent."
+            case .Pisces:
+                return "Intuitive, compassionate, artistic, gentle, wise, musical."
+        }
+    }
+
+    public func weaknesses()->String {
+        switch self {
+            case .Aries:
+                return "Impatience, silly arguments, allowing fear to limit choices."
+            case .Taurus:
+                return "Stubborn, uncompromising, possessive."
+            case .Gemini:
+                return "Scattering energy in too many places at once, fickle in love, nervous, short attention span."
+            case .Cancer:
+                return "Manipulative, indirect conflict, clinging to the past, insecure, packrat."
+            case .Leo:
+                return "Arrogance, stubbornness, inflexibility, self-centeredness, laziness."
+            case .Virgo:
+                return "Worry, shyness, overly critical of self and others, all work and no play."
+            case .Libra:
+                return "Indecisive, will carry a grudge, avoids confrontations, self-pity"
+            case .Scorpio:
+                return "Jealous, distrusting, secretive, violent, caustic"
+            case .Sagittarius:
+                return "Will say anything no matter how undiplomatic, promises more than can deliver, can be impatient to the point of rudeness."
+            case .Capricorn:
+                return "Know-it-all, unforgiving, condescending, expecting the worst."
+            case .Aquarius:
+                return "Runs from emotional expression, uncompromising, temperamental, aloof."
+            case .Pisces:
+                return "Can be a victim or martyr, fearful, overly trusting, sad, desire to escape reality."
         }
     }
 }
